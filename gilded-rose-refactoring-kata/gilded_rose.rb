7223764d586a2ac0a77d64676a0c024a2bbb1b8e
Valentin Boyanov <valentin@onestic.com>
2020-04-18 12:35:29 +0200

    extract expired logic

--- gilded-rose-refactoring-kata/gilded_rose.rb
+++ gilded-rose-refactoring-kata/gilded_rose.rb
@@ -31,19 +31,7 @@ class GildedRose
     end
     item.sell_in = item.sell_in - 1
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
+      update_expired_item item
     end
   end
 
@@ -51,6 +39,22 @@ class GildedRose
     @items.each do |item|
       update_item(item)
     end  
+  end
+
+  def update_expired_item item
+    if item.name != "Aged Brie"
+      if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          item.quality = item.quality - 1
+        end
+      else
+        item.quality = item.quality - item.quality
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
   end
 end
 
