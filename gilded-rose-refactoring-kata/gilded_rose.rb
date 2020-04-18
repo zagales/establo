afd4d507c15f895f54298b98ba43f8007688b42a
Valentin Boyanov <valentin@onestic.com>
2020-04-18 12:56:51 +0200

    extract main checks

--- gilded-rose-refactoring-kata/gilded_rose.rb
+++ gilded-rose-refactoring-kata/gilded_rose.rb
@@ -4,31 +4,20 @@ class GildedRose
     @items = items
   end
 
-  def update_item(item)
-    if item.name == "Sulfuras, Hand of Ragnaros"
-      return
-    end   
+  def update_quality()
+    @items.each do |item|
+      update_item(item)
+    end  
+  end  
 
-    if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"      
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-      end
+  def update_item item
+    if is_legendary_item item
+      return
+    end
+    if is_item_that_increase_quality_with_time item
+      increase_item_quality item
     else
-      if item.quality > 0
-        item.quality = item.quality - 1
-      end    
+      decrease_item_quality item
     end
     item.sell_in = item.sell_in - 1
     if item.sell_in < 0
@@ -36,10 +25,36 @@ class GildedRose
     end
   end
 
-  def update_quality()
-    @items.each do |item|
-      update_item(item)
-    end  
+  def is_legendary_item item
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def is_item_that_increase_quality_with_time item
+    item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def increase_item_quality item
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+
+  def decrease_item_quality item
+    if item.quality > 0
+      item.quality = item.quality - 1
+    end 
   end
 
   def update_expired_item item
