90195e4a78d6e20eef2b03255092fe529f2366fb
Valentin Boyanov <valentin@onestic.com>
2020-04-18 12:12:21 +0200

    extract sulfuras case as guardclause

--- gilded-rose-refactoring-kata/gilded_rose.rb
+++ gilded-rose-refactoring-kata/gilded_rose.rb
@@ -5,11 +5,12 @@ class GildedRose
   end
 
   def update_item(item)
+    if item.name == "Sulfuras, Hand of Ragnaros"
+      return
+    end
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
-        end
+        item.quality = item.quality - 1
       end
     else
       if item.quality < 50
@@ -28,16 +29,12 @@ class GildedRose
         end
       end
     end
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
+    item.sell_in = item.sell_in - 1
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
+            item.quality = item.quality - 1
           end
         else
           item.quality = item.quality - item.quality
