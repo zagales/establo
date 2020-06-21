b7247d9f292c0fb5cefa2c291631af5bcb46e647
Rafael Esteller <rafael.esteller@playsportsnetwork.com>
2020-06-21 11:50:24 +0100

    Extract parser to its own class

--- racing-car-kata/php/tests/TextConverter/HtmlPagesConverterTest.php
+++ racing-car-kata/php/tests/TextConverter/HtmlPagesConverterTest.php
@@ -6,14 +6,16 @@ namespace Tests\TextConverter;
 
 use PHPUnit\Framework\TestCase;
 use RacingCar\TextConverter\HtmlPagesConverter;
+use RacingCar\TextConverter\PageBreakFileParser;
 
 class HtmlPagesConverterTest extends TestCase
 {  
     public function testGetFirstHtmlPage(): void
     {
         $expectedHtmlContent = 'page1 line1<br />page1 line2<br />';
-        
-        $converter = new HtmlPagesConverter(__DIR__ . '/files/unicodePaginatedFile.txt');
+        $pageBreakParser = new PageBreakFileParser(__DIR__ . '/files/unicodePaginatedFile.txt');        
+        $converter = new HtmlPagesConverter(__DIR__ . '/files/unicodePaginatedFile.txt', $pageBreakParser);
+
         $htmlResult = $converter->getHtmlPage(1);
         
         $this->assertEquals($expectedHtmlContent, $htmlResult);        
@@ -21,8 +23,8 @@ class HtmlPagesConverterTest extends TestCase
 
     public function testThrowExceptionOnNonExistingPages(): void
     {
-        
-        $converter = new HtmlPagesConverter(__DIR__ . '/files/unicodePaginatedFile.txt');
+        $pageBreakParser = new PageBreakFileParser(__DIR__ . '/files/unicodePaginatedFile.txt');        
+        $converter = new HtmlPagesConverter(__DIR__ . '/files/unicodePaginatedFile.txt', $pageBreakParser);
       
         $this->expectException(\Exception::class);
         $requestedPage = 6;
